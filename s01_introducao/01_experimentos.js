210c64fadc66d3e98afeed5b5b725d51af0da6d7
Cleverson Rocha <cleverson.rocha@minu.co>
2022-01-21 09:56:33 -0300

    Inicio do curso

---
+++ s01_introducao/01_experimentos.js
@@ -0,0 +1,39 @@
+function esperarPor(tempo) {
+  const futuro = Date.now() + tempo;
+  while (Date.now() < futuro) { };
+};
+
+setTimeout(() => console.log('Exec #01'), 400);
+setTimeout(() => {
+  esperarPor(3000)
+  console.log('Exec #02');
+}, 300);
+esperarPor(5000);
+console.log('Fim');
+
+
+
+// function fn5(){
+//   console.log('timeout');
+// };
+
+// function fn4(){
+// fn5();
+// };
+
+// function fn3() {
+//   setTimeout(() => fn4(), 3000)
+//   console.log('fn3');
+// };
+// function fn2() {
+//   fn3();
+// };
+// function fn1() {
+//   fn2();
+// };
+// function main() {
+//   fn1()
+// }
+
+// main()
+// console.log('Fim')
---
+++ s02_funcoes/01_funcao.js
@@ -0,0 +1,29 @@
+let a = 4;
+console.log(a);
+
+
+// Function Declaration
+function bomDia() {
+  console.log('Bom dia!');
+
+};
+
+bomDia();
+
+
+function somar(a, b) {
+  return a + b;
+
+};
+
+const resultado = somar(3, 4);
+console.log(resultado);
+
+
+// Function expression
+const boaTarde = function () {
+  console.log('Boa tarde!');
+
+};
+
+boaTarde();
