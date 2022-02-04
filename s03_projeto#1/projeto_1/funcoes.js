cc2f43c694c8eedf2c971bfcd40692bfe5991a27
Cleverson Rocha <cleverson.rocha@minu.co>
2022-02-04 10:23:52 -0300

    Projeto#1 - Finalizado

--- s03_projeto#1/projeto_1/funcoes.js
+++ s03_projeto#1/projeto_1/funcoes.js
@@ -5,9 +5,11 @@ const path = require('path')
 function lerDiretorio(caminho) {
   return new Promise((resolve, reject) => {
     try {
-      let arquivos = fs.readdirSync(caminho)
-      arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
-      resolve(arquivos)
+      const arquivos = fs.readdirSync(caminho)
+      const arquivosCompletos = arquivos.map(arquivo => {
+        return path.join(caminho, arquivo)
+      })
+      resolve(arquivosCompletos)
     } catch (e) {
       reject(e)
     }
@@ -29,23 +31,64 @@ function lerArquivos(caminhos) {
   return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padraoTextual) {
-  return array.filter(el => el.endsWith(padraoTextual))
+function elementosTerminadosCom(padraoTextual) {
+  return function (array) {
+    return array.filter(el => el.endsWith(padraoTextual))
+  }
 }
 
-function removerSeVazio(array) {
+function removerElementosSeVazio(array) {
   return array.filter(el => el.trim())
 }
 
-function removerSeIncluir(array, padraoTextual) {
-  return array.filter(el => !el.includes(padraoTextual))
+function removerElementosSeIncluir(padraoTextual) {
+  return function (array) {
+    return array.filter(el => !el.includes(padraoTextual))
+  }
 }
 
-function removerSeApenasNumero(array) {
+function removerElementosSeApenasNumero(array) {
   return array.filter(el => {
     const num = parseInt(el.trim())
     return num !== num
   })
+}
+
+function removerSimbolos(simbolos) {
+  return function (array) {
+    return array.map(el => {
+      return simbolos.reduce((acc, simbolo) => {
+        return acc.split(simbolo).join('')
+      }, el)
+    })
+  }
+}
+
+function mesclarElementos(array) {
+  return array.join(' ')
+}
+
+function separarTextoPor(simbolo) {
+  return function (texto) {
+    return texto.split(simbolo)
+  }
+}
+
+function agruparElementos(palavras) {
+  return Object.values(palavras.reduce((acc, palavra) => {
+    const el = palavra.toLowerCase()
+    const qtde = acc[el] ? acc[el].qtde + 1 : 1
+    acc[el] = { elemento: el, qtde }
+    return acc
+  }, {}))
+}
+
+function ordenarPorAtributoNumerico(attr, ordem = 'asc') {
+  return function (array) {
+    const asc = (obj1, obj2) => obj1[attr] - obj2[attr]
+    const desc = (obj1, obj2) => obj2[attr] - obj1[attr]
+    return array.sort(ordem === 'asc' ? asc : desc)
+  }
 }
 
 module.exports = {
@@ -53,7 +96,12 @@ module.exports = {
   lerArquivo,
   lerArquivos,
   elementosTerminadosCom,
-  removerSeVazio,
-  removerSeIncluir,
-  removerSeApenasNumero
+  removerElementosSeVazio,
+  removerElementosSeIncluir,
+  removerElementosSeApenasNumero,
+  removerSimbolos,
+  mesclarElementos,
+  separarTextoPor,
+  agruparElementos,
+  ordenarPorAtributoNumerico
 };
--- s03_projeto#1/projeto_1/index.js
+++ s03_projeto#1/projeto_1/index.js
@@ -3,13 +3,24 @@ const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
+const simbolos = [
+  '.', '?', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')'
+]
+
 fn.lerDiretorio(caminho)
-  .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-  .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
-  .then(conteudos => conteudos.join('\n'))
-  .then(todoConteudo => todoConteudo.split('\n'))
-  .then(linhas => fn.removerSeVazio(linhas))
-  .then(linhas => fn.removerSeIncluir(linhas, '-->'))
-  .then(linhas => fn.removerSeApenasNumero(linhas))
+  .then(fn.elementosTerminadosCom('.srt'))
+  .then(fn.lerArquivos)
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor('\n'))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeIncluir('-->'))
+  .then(fn.removerElementosSeApenasNumero)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor(' '))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeApenasNumero)
+  .then(fn.agruparElementos)
+  .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
 
   .then(console.log)
