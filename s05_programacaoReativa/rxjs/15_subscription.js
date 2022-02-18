05f2903f63784b61b047453be3cb3f6059b9f049
Cleverson Rocha <cleverson.rocha@minu.co>
2022-02-18 13:39:56 -0300

    RxJS

---
+++ s05_programacaoReativa/rxjs/15_subscription.js
@@ -0,0 +1,25 @@
+/*
+ Esperar 3000
+ Gerar números de 500
+ Depois de 5000s -> chamar o unsubscribe
+ */
+
+const { timer, Subscription } = require('rxjs')
+
+const obs = timer(3000, 500)
+
+const sub1 = obs.subscribe(num => {
+  console.log(`#1 Gerou o número ${num}`)
+})
+
+const sub2 = obs.subscribe(num => {
+  console.log(`#2 Gerou o número ${num}`)
+})
+
+const sub = new Subscription()
+sub.add(sub1)
+sub.add(sub2)
+
+setTimeout(() => {
+  sub.unsubscribe()
+}, 5000)
